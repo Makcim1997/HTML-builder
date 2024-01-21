cbcf8fa934af1557288af47d432678d1bc33b43f
Makcim <94489688+Makcim1997@users.noreply.github.com>
2024-01-21 18:38:07 +0300

    feat: File read and write tasks completed

---
+++ 02-write-file/index.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+const { stdin, stdout } = process;
+
+const pathToFile = path.join(__dirname, 'text.txt');
+
+const exit = () => {
+    stdout.write('Работа завершена!\n');
+    process.exit();
+}
+
+const fileHandler = () => {
+    fs.open(pathToFile, 'w', (err) => {
+        if (err) throw err;
+    })
+    stdout.write('Введите текст\n');
+    stdin.on('data', data => {
+        const stringDate = data.toString();
+
+        if (stringDate.includes('.exit')) {
+            exit();
+        }
+
+        fs.appendFile(pathToFile, data, (err) => {
+            if (err) throw err;
+        })
+    })
+}
+
+process.on('SIGINT', () => {});
+process.on('SIGINT', exit);
+
+fileHandler();
